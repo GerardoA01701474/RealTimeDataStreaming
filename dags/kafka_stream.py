11628c1f87b0a58649c1eea7e34ee9dfb22ef02e
gera <gerardopp09@gmail.com>
2024-12-03 19:00:01 -0600

    new commit

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -30,12 +30,17 @@ def format_data(res):
     data['registered_date'] = res['registered']['date']
     data['phone'] = res['phone']
     data['picture'] = res['picture']['medium']
+    return(data)
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
     res = get_data()
     res = format_data(res)
-    print(json.dumps(res, indent=3))
+    # print(json.dumps(res, indent=3))
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
     
 
 with DAG('user_automation',
